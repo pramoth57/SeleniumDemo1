7c47b8442202cee92f7ba51b494ff384ad2994da
Pramoth Kumar M <Pramothkumar57@gmail.com>
2016-11-02 00:27:57 -0700

    Test

---
+++ src/mavenDemo/DemoTestNG.java
@@ -0,0 +1,31 @@
+package mavenDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class DemoTestNG {
+
+	public static void main(String[] args) throws InterruptedException {
+         WebDriver h= new FirefoxDriver();
+		
+		//HtmlUnitDriver h = new HtmlUnitDriver();
+		h.get("http://www.google.com/");
+		WebElement e = h.findElement(By.xpath("//span[text() = 'Tutorial']"));
+		
+		Actions a = new Actions(h);
+		a.moveToElement(e).perform();
+		
+		System.out.println(" test Complete");
+		
+		Thread.sleep(5000);
+		
+		h.quit();
+		
+
+	}
+
+}
